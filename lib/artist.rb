6e9231d6f5b3b88137cd71d53d692d71d571824d
stacy <stacydn23@gmail.com>
2020-11-17 23:39:06 -0500

    commit tree eb2e86b0364f7a433a25887a2a1d92dda7802c6a

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,30 @@
+class Artist
 
+    attr_accessor :name, :songs, :genres
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+        @genres = []
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end 
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end 
+
+    def genres
+       songs.map(&:genre)
+    end 
+
+end 
